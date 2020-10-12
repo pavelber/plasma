aad6b8b343f6e99016c6386388103b4b75d9d53f
Pavel Bernshtam <pavelb@cgen.com>
2020-10-12 21:09:12 +0300

    temp2

--- create_qsege.py
+++ create_qsege.py
@@ -10,12 +10,8 @@ def error(s):
     exit(1)
 
 
-if len(sys.argv) != 2:
-    error('Usage: ' + sys.argv[0] + ' path_to_IN1R.INP path_to_fac_files_dir')
-
-
 def print_header():
-    print HEADER_FORMAT_STRING % 'SpS  QSs   AI    FAC PI   NIST PI'
+    print HEADER_FORMAT_STRING % ('SpS', 'QSs', 'AI', 'FAC PI', 'NIST PI')
     print '------------------------------------------'
 
 
@@ -24,29 +20,37 @@ def skip_n_lines(f, num):
         next(f)
 
 
-def skip_lines(inp):
-    skip_n_lines(inp, 13)
+def skip_lines(f):
+    skip_n_lines(f, 13)
 
 
-def copy_lines(inp):
-    for line in inp:
+def copy_lines(f):
+    for line in f:
         columns = line.split()
         if len(columns) == 7:
-            print(line)
+            print(line),
         else:
             break
 
+
+def copy_atomic(f):
+    for line in f:
+        columns = line.split()
+        if len(columns) == 7:
+            print HEADER_FORMAT_STRING % (columns[0], columns[1], columns[2], columns[4], columns[6])
+
+        else:
+            print(line),
+
+
+if len(sys.argv) != 3:
+    error('Usage: ' + sys.argv[0] + ' path_to_IN1R.INP path_to_fac_files_dir')
 
 if os.path.exists(sys.argv[1]):
     with open(sys.argv[1], 'rb') as inp:
         print_header()
         skip_lines(inp)
         copy_lines(inp)
-        for line in inp:
-            columns = line.split()
-            if len(columns) == 1:
-                print(line)
-            else:
-                print HEADER_FORMAT_STRING % (columns[0], columns[1], columns[2], columns[4], columns[6])
+        copy_atomic(inp)
 else:
     error('Can\'t open file ' + sys.argv[1])
