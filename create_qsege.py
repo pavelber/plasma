9a7da1d133c43d3f53f264bfae8ce843e4b8121d
Pavel Bernshtam <pavelb@cgen.com>
2020-10-16 15:31:31 +0300

    all except autoiniaz and electrons

--- create_qsege.py
+++ create_qsege.py
@@ -1,8 +1,10 @@
+import csv
 import os
 import sys
 
-HEADER_FORMAT_STRING = ' %2s %3s %3s %8s %8s'
-OUTPUT_FORMAT_STRING = ' %2s %3s %3s %5.2f %5.2f'
+HEADER_FORMAT_STRING = '%3s  %3s  %3s %8s %8s'
+OUTPUT_FORMAT_STRING = '%24s%5s%13s%7d%9d'
+OUTPUT_FORMAT_STRING2 = '%24s%5s%13s%7d%9d%9s%15s'
 
 
 def error(s):
@@ -21,36 +23,96 @@ def skip_n_lines(f, num):
 
 
 def skip_lines(f):
-    skip_n_lines(f, 13)
+    skip_n_lines(f, 12)
 
 
-def copy_lines(f):
+def copy_lines(f, element):
+    el = int(name_to_table[element]["AtomicNumber"])
     for line in f:
         columns = line.split()
         if len(columns) == 7:
-            print(line),
+            num = el - int(columns[0]) + 1
+            if num == 0:
+                break
+            name = num_to_table[str(num)]["Symbol"]
+            print HEADER_FORMAT_STRING % (columns[0], columns[1], columns[2], columns[4], columns[6]),
+            print(" [" + name + "]")
         else:
             break
 
 
-def copy_atomic(f):
+def copy_atomic(f, element):
+    el = int(name_to_table[element]["AtomicNumber"])
+    counter = 1
+    block_counter = 1
+    autoionization = False
+
     for line in f:
         columns = line.split()
-        if len(columns) == 7:
-            print HEADER_FORMAT_STRING % (columns[0], columns[1], columns[2], columns[4], columns[6])
-
+        if len(columns) == 1:
+            autoionization = False
+            e = columns[0]
+            num = el - int(columns[0]) + 1
+            if num == 0:
+                break
+            name = num_to_table[str(num)]["Symbol"]
+            if counter == 1:  # first time
+                print(e + " [" + name + "]" + "                    g0        E(eV)       #       ##")
+            else:
+                print(e + " [" + name + "]")
+            block_counter = 1
+        elif len(columns) == 7:
+            if not autoionization:
+                print OUTPUT_FORMAT_STRING % (
+                    columns[0] + " " + columns[1], columns[2], columns[3], block_counter, counter)
+                counter += 1
+                block_counter += 1
+        elif len(columns) == 9:
+            if not autoionization:
+                print OUTPUT_FORMAT_STRING2 % (
+                    columns[0] + " " + columns[1], columns[2], columns[3], block_counter, counter, columns[7],
+                    columns[8])
+                counter += 1
+                block_counter += 1
+        elif len(columns) == 2:
+            autoionization = True
         else:
             print(line),
 
 
-if len(sys.argv) != 3:
-    error('Usage: ' + sys.argv[0] + ' path_to_IN1R.INP path_to_fac_files_dir')
+def read_element(inp):
+    line = inp.readline()
+    columns = line.split()
+    return columns[0]
+
+
+def read_table():
+    dict1 = {}
+    dict2 = {}
+    with open(os.path.dirname(__file__) + os.path.sep + "PeriodicTable.csv", 'rb') as infile:
+        reader = csv.reader(infile)
+        headers = next(reader)[0:]
+        for row in reader:
+            dict1[row[2]] = {key: value for key, value in zip(headers, row[0:])}
+            dict2[row[0]] = {key: value for key, value in zip(headers, row[0:])}
+    return dict1, dict2
+
+
+if len(sys.argv) != 2:
+    error('Usage: ' + sys.argv[0] + ' path_to_IN1R.INP')
+
+(name_to_table, num_to_table) = read_table()
+
+print os.path.abspath(__file__)
+print os.path.dirname(__file__)
 
 if os.path.exists(sys.argv[1]):
     with open(sys.argv[1], 'rb') as inp:
+        element = read_element(inp)
         print_header()
         skip_lines(inp)
-        copy_lines(inp)
-        copy_atomic(inp)
+        copy_lines(inp, element)
+        print "----------------------------------------------------------------"
+        copy_atomic(inp, element)
 else:
     error('Can\'t open file ' + sys.argv[1])
