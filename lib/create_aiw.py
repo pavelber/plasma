39d12c82cc4535b96db716cb76b6ad7ff343467a
Pavel Bernshtam <pavel@valapay.com>
2021-02-14 18:36:42 +0200

    aiw

--- lib/create_aiw.py
+++ lib/create_aiw.py
@@ -3,7 +3,7 @@ import os
 
 def copy_for_spectroscopic_numbers(outf, out_dir, spec_numbers, translation_table):
     for n in spec_numbers:
-        translation = translation_table[n]
+        n_next = str(int(n)+1)
         in_path = os.path.join(out_dir, n, "fac.ai")
         if os.path.exists(in_path):
             with open(in_path, 'rb') as inf:
@@ -11,11 +11,12 @@ def copy_for_spectroscopic_numbers(outf, out_dir, spec_numbers, translation_tabl
                     parts = line.split()
                     if len(parts) == 7:
                         lev1_search = str(int(parts[0]) + 1)
-                        lev2_search = str(int(parts[0]) + 1)
-                        if lev1_search in translation and lev2_search in translation:
-                            lev1 = translation[lev1_search]
-                            lev2 = translation[lev2_search]
-                            outf.write("%5s%5s%5d%5s%15s%15s\n" % (n, lev1, (int(n) + 1), lev2, parts[5], parts[6]))
+                        lev2_search = str(int(parts[2]) + 1)
+                        if lev1_search in translation_table[n]:
+                            lev1 = translation_table[n][lev1_search]
+                            max_level = max(map(lambda x: int(x), translation_table[n].keys()))
+                            lev2 = str(int(lev2_search)-max_level)
+                            outf.write("%5s%5s%5s%5s%15s%15s\n" % (n, lev1, n_next, lev2, parts[5], parts[4]))
                         else:
                             print ("No " + lev1_search + " or " + lev2_search)
 
