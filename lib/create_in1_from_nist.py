b18fc2cf74b772a7b5d88eff9519ca6dd38ab782
Pavel Bernshtam <pavel@valapay.com>
2023-01-29 11:35:29 +0200

    num of levels bug fixed

--- lib/create_in1_from_nist.py
+++ lib/create_in1_from_nist.py
@@ -35,7 +35,7 @@ def find_previous(c):
     else:
         config_wo_electrons = c
     index = levels_order.index(config_wo_electrons)
-    return levels_order[index+1]+str(level_to_electrons[levels_order[index+1]])
+    return levels_order[index + 1] + str(level_to_electrons[levels_order[index + 1]])
 
 
 def write_section(elem, outf, spec_num, spec_num_file, data_file, energy_limits):
@@ -48,13 +48,13 @@ def write_section(elem, outf, spec_num, spec_num_file, data_file, energy_limits)
             if not line.startswith('"=""' + elem):
                 parts = line.strip().split(',')
                 configuration = nist_strip(parts[0])
-                if len(configuration)==0:
+                if len(configuration) == 0:
                     continue
                 configs = format_configuration(configuration, 10)
                 if len(configs) == 2:
                     config = " ".join(configs)
                 else:
-                    config = find_previous(configs[0])+" "+configs[0]
+                    config = find_previous(configs[0]) + " " + configs[0]
                 term = format_term(nist_strip(parts[1]))
                 g = nist_strip(parts[3])
                 energy_str = clean_num(nist_strip(parts[eV_column]))
@@ -63,7 +63,7 @@ def write_section(elem, outf, spec_num, spec_num_file, data_file, energy_limits)
                     outf.write("\n")
                     return level
                 else:
-                    if energy_limits[spec_num] > float(energy_str):
+                    if energy_limits > float(energy_str):
                         outf.write(" %-10s%8s%3s%14.3f    0.00e+00 0.00e+00% 6d\n" % (config, term, g, level, n))
                         data_file.write("%s,%d,%s\n" % (spec_num, n, energy_str))
                         n = n + 1
@@ -86,19 +86,20 @@ def read_section(elem, spec_num_file):
         return level
 
 
-def read_section_pass2(elem, spec_num_file, energy):
+def read_section_pass2(elem, spec_num_file, energy, max_energy):
     with open(spec_num_file, "rb") as inf:
         inf.readline()
         levels = 0
         ai_levels = 0
         for line in inf:
-            if not line.startswith('"=""' + elem):
+            if not line.startswith('"=""' + elem) and not line.startswith('"="""""'):
                 parts = line.strip().split(',')
-                level = float(clean_num(nist_strip(parts[3])))
-                if level < energy:
-                    levels += 1
-                else:
-                    ai_levels += 1
+                level_energy = float(clean_num(nist_strip(parts[5])))
+                if level_energy < max_energy:
+                    if level_energy < energy:
+                        levels += 1
+                    else:
+                        ai_levels += 1
         return levels, ai_levels
 
 
@@ -116,7 +117,7 @@ def create_header(i_spectro, elem, table, in1_inp, spec_number_energy, levels_da
     in1_inp.write("   or     A / (B + C*t + D*t*t)  D = 0.00000D+00\n")
     in1_inp.write("EDens (cm-3) = A + B*t + C*t*t : A = 5.00000D+20; B = 0.00000D+00; C = 0.00000D+15\n")
     in1_inp.write("   or     A / (B + C*t + D*t*t)  D = 0.00000D-03\n")
-    in1_inp.write("Step= 1.0D-09 sec:No of steps="+str(len(i_spectro))+"\n")
+    in1_inp.write("Step= 1.0D-09 sec:No of steps=" + str(len(i_spectro)) + "\n")
 
     for n in i_spectro:
         in1_inp.write("%3d %4d %4d  0 %8.2f    12  %8.2f   0.0000   0.0000   0.000\n" % (
@@ -144,10 +145,11 @@ def create_in1_inp_from_nist(dir, elem, energy_limits):
                 spec_number_energy[f] = read_section(elem, csv_path)
 
             for f in i_spectro:
-                levels_data[f] = read_section_pass2(elem, os.path.join(dir, str(f) + '.csv'), spec_number_energy[f])
+                levels_data[f] = read_section_pass2(elem, os.path.join(dir, str(f) + '.csv'), spec_number_energy[f],
+                                                    energy_limits[str(f)])
 
             create_header(i_spectro, elem, table, in1_inp, spec_number_energy, levels_data)
 
             for f in i_spectro:
-                write_section(elem, in1_inp, str(f), os.path.join(dir, str(f) + '.csv'), in1_csv, energy_limits)
+                write_section(elem, in1_inp, str(f), os.path.join(dir, str(f) + '.csv'), in1_csv, energy_limits[str(f)])
             return i_spectro
