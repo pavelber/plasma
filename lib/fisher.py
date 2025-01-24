2a2290879726c4f4eae3f4ca1cdf6ed62868476e
Pavel Bernshtam <pavel@anymaint.com>
2025-01-24 15:52:11 +0200

    fisher better inz

--- lib/fisher.py
+++ lib/fisher.py
@@ -15,44 +15,60 @@ def compute_energy_diff(spectr_num_low, level_low, spectr_num_high, level_high, 
 
 
 def createIonFile(dont_run_all_tools, element, levels_num, o_dir, spectr_num_to_aion_energy,
-                  spectr_num_level_to_energy, bcfp):
+                  spectr_num_level_to_energy, rrec):
     dir_path = os.path.join(o_dir, "fisher")
-    in_file_path_1 = os.path.join(o_dir, bcfp)
-    in_file_path_2 = os.path.join(o_dir, "RREC.INP")
+    bcfp_file_path = os.path.join(o_dir, rrec)
+    rrec_file_path = os.path.join(o_dir, "RREC.INP")
     out_file_path = os.path.join(dir_path, "Inz" + element + levels_num + ".INP")
     print("Creation of " + out_file_path)
-    levels_to_bcfp = {}
-    with open(in_file_path_1, "r") as inf1:
-        for line1 in inf1:
-            parts1 = line1.split()
-            if len(parts1) > 4:
-                id_by = (parts1[0], parts1[1], parts1[3])
-                levels_to_bcfp[id_by] = line1
-    with open(in_file_path_2, "r") as inf2:
-        skip_n_lines(inf2, 2)
-        with open(out_file_path, "w") as outf:
-            outf.write(HEADER)
-            for line2 in inf2:
-                parts2 = line2.split()
-                id_by = (parts2[0], parts2[1], parts2[2])
-                if not id_by in levels_to_bcfp:
-                    print(id_by)
-                else:
-                    bcfp_parts = levels_to_bcfp[id_by].split()
-                    spectr_num_low = bcfp_parts[0]
-                    level_low = bcfp_parts[1]
-                    spectr_num_high = bcfp_parts[2]
-                    level_high = bcfp_parts[3]
-                    bsfp = " %4s %4s %4s %4s %14s %15s %15s %15s" % (
-                        spectr_num_low, level_low, spectr_num_high, level_high, bcfp_parts[4], bcfp_parts[5],
-                        bcfp_parts[6], bcfp_parts[7])
-                    if (spectr_num_high, level_high) in spectr_num_level_to_energy:
-                        energy = compute_energy_diff(spectr_num_low, level_low, spectr_num_high, level_high,
-                                                     spectr_num_to_aion_energy,
-                                                     spectr_num_level_to_energy)
-                        rrec = " %6s %13s %12s %12s %12s %13.3f" % (
-                            parts2[3], parts2[4], parts2[5], parts2[6], parts2[7], energy)
-                        outf.write(bsfp + rrec + "\n")
+    levels_to_bcfp_rrec = {}
+    with open(bcfp_file_path, "r") as bcfp_f:
+        skip_n_lines(bcfp_f, 2)
+        for line in bcfp_f:
+            parts = line.split()
+            if len(parts) > 4:
+                level = (parts[0], parts[1], parts[3])
+                if parts[1]!="0":
+                    levels_to_bcfp_rrec[level] = (line, None)
+
+    with open(rrec_file_path, "r") as rrec_f:
+        skip_n_lines(rrec_f, 2)
+        for line in rrec_f:
+            parts = line.split()
+            level = (parts[0], parts[1], parts[2])
+            if level in levels_to_bcfp_rrec:
+                levels_to_bcfp_rrec[level] = (levels_to_bcfp_rrec[level][0], line)
+            else:
+                levels_to_bcfp_rrec[level] = (None, line)
+    sorted_levels = sorted(levels_to_bcfp_rrec.keys(), key=lambda x: tuple(map(int, x)))
+
+    with open(out_file_path, "w") as outf:
+        outf.write(HEADER)
+        for level in sorted_levels:
+            (bcfp_line, rrec_line) = levels_to_bcfp_rrec[level]
+            spectr_num_low = level[0]
+            level_low = level[1]
+            spectr_num_high = str(int(spectr_num_low)+1)
+            level_high = level[2]
+            if bcfp_line is None:
+                bcfp_line = "0.000E+00 0.000E+00 0.000E+00 0.000E+00 0.000E+00 0.000E+00 0.000E+00 0.000E+00"
+                print("BCFP line is None for " + str(level))
+            bcfp_parts = bcfp_line.split()
+            bcfp_fit = " %4s %4s %4s %4s %14s %15s %15s %15s" % (
+                spectr_num_low, level_low, spectr_num_high, level_high, bcfp_parts[4], bcfp_parts[5],
+                bcfp_parts[6], bcfp_parts[7])
+            if (spectr_num_high, level_high) in spectr_num_level_to_energy and rrec_line is not None:
+                energy = compute_energy_diff(spectr_num_low, level_low, spectr_num_high, level_high,
+                                             spectr_num_to_aion_energy,
+                                             spectr_num_level_to_energy)
+            else:
+                print("RR line is None for " + str(level))
+                energy = 0.0
+                rrec_line = "0 0 0 4 0.000E+00 0.000E+00 0.000E+00 0.000E+00"
+            rrec_parts = rrec_line.split()
+            rrec_fit = " %6s %13s %12s %12s %12s %13.3f" % (
+                    rrec_parts[3], rrec_parts[4], rrec_parts[5], rrec_parts[6], rrec_parts[7], energy)
+            outf.write(bcfp_fit + rrec_fit + "\n")
 
 
 def run_qsege(dont_run_all_tools, min_sp_num, max_sp_num, element, o_dir, use_fac_lev=True):
@@ -105,7 +121,7 @@ def run_for_fisher(dont_run_all_tools, min_sp_num, max_sp_num, element, o_dir, b
                    use_fac_lev=True):
     path_to_in1_inp = os.path.join(o_dir, "IN1.INP")
     (spectr_num_to_aion_energy, spectr_num_level_to_energy) = get_energy_from_in1_inp(path_to_in1_inp)
-    next_sp_num = str(int(max_sp_num)+1)
+    next_sp_num = str(int(max_sp_num) + 1)
     if (next_sp_num, "1") not in spectr_num_level_to_energy:
         spectr_num_level_to_energy[(next_sp_num, "1")] = 1.0
     levels_num = run_qsege(dont_run_all_tools, min_sp_num, max_sp_num, element, o_dir, use_fac_lev)
