8cbc704ef3a1470fa4899c4733798562dcef13b3
Pavel Bernshtam <pavel@anymaint.com>
2025-02-21 14:56:17 +0200

    fix renumerate

--- fisher-renumerate/renumerate-aiw-200.py
+++ fisher-renumerate/renumerate-aiw-200.py
@@ -1,6 +1,5 @@
 import sys
 
-
 from util import process_line, ConversionType
 
 
@@ -8,7 +7,7 @@ def process_file(input_file, conversion_type):
     with open(input_file, 'r') as infile:
         lines = infile.readlines()
 
-    processed_lines = [process_line(line,7, 11, conversion_type) for line in lines]
+    processed_lines = [process_line(line, 6, 10, conversion_type) for line in lines]
     return ''.join(processed_lines)
 
 
@@ -24,4 +23,3 @@ if __name__ == "__main__":
         conversion_type = ConversionType.NEGATIVE_TO_200
     processed_data = process_file(input_filename, conversion_type)
     print(processed_data)
-
